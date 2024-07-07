2985ac0f8ae0a69f5c50e503165f130c4c5411f0
Shivam Srivastava <shivam.362000.1@gmail.com>
2024-07-07 05:45:14 +0530

    Socket.io instantiated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const { json, urlencoded } = express;
+// const { swaggerUi, swaggerDocs } = require('./config/swagger');
 
 const app = express();
 
@@ -9,17 +11,23 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
 // Database connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log('MongoDB connection error:', err));
+
+// Swagger setup
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notifications', require('./routes/notifications'));
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,37 @@
 const app = require('./app');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+// const io = socketIo(server);
+
+// Handle WebSocket connections
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('error', (err) => {
+    console.error('Socket error:', err);
+  });
+
+  socket.on('connect_error', (err) => {
+    console.error('Connect error:', err);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = { server, io };
