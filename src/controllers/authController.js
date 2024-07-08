1462553c0635512efb246078c87c3f4ec09225b3
Shivam Srivastava <shivam.362000.1@gmail.com>
2024-07-09 02:26:40 +0530

    GET pagination added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, role, password } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
@@ -11,7 +11,7 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const user = new User({ username, email, password });
+    const user = new User({ username, email, role, password });
     await user.save();
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
---
+++ src/middlewares/roleMiddleware.js
@@ -0,0 +1,8 @@
+const roleMiddleware = async (req, res, next) => {
+    if (!req.user || req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+};
+
+module.exports = roleMiddleware;
